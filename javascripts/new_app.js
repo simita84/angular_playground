24e9ac359f300857b730696933f9de10029d9f54
john_supsupin <jsupsupin@village88.com>
2015-04-08 04:15:42 -0700

    directives

--- javascripts/new_app.js
+++ javascripts/new_app.js
@@ -1,6 +1,93 @@
 var tasks= angular.module('tasksApp', [])
 
-//controller
+tasks.directive("superlist", function(){
+	return {
+		restrict: "E",
+		template: "<div>ALL TASKS.<div>"
+	}
+})
+
+tasks.directive("special", function(){
+	return {
+		restrict: "A",
+		link: function(){
+			alert("List of task is loaded.")
+		}
+	}
+})
+
+tasks.directive("taskcategories", function(){
+	return {
+		restrict: "E",
+
+		controller: function ($scope){
+			$scope.categories = []
+
+			this.addMajor = function(){
+				$scope.categories.push("major")
+			}
+
+			this.addMinor = function(){
+				$scope.categories.push("minor")
+			}
+		},
+
+		link: function(scope, element){
+			element.bind("mouseenter", function(){
+				console.log(scope.categories)
+			})
+		}
+	}
+})
+
+tasks.directive("major", function(){
+	return {
+		require: "taskcategories",
+		link: function(scope, element, attrs, taskcategories){
+			taskcategories.addMajor();
+		}
+	}
+})
+
+tasks.directive("minor", function(){
+	return {
+		require: "taskcategories",
+		link: function(scope, element, attrs, taskcategories){
+			taskcategories.addMinor();
+		}
+	}
+})
+
+//directive with mouse event handling
+tasks.directive("enter", function(){
+	return function(scope, element, attrs){
+		element.bind("mouseenter", function(){
+			//sample events
+			element.removeClass(attrs.leave)
+			element.addClass(attrs.enter)
+		})
+	}
+})
+
+tasks.directive("leave", function(){
+	return function(scope, element, attrs){
+		element.bind("mouseleave", function(){
+			//sample events
+			element.removeClass(attrs.enter)
+			element.addClass(attrs.leave)
+		})
+	}
+})
+
+//directive calling function in a controller
+tasks.directive("callmethod", function(){
+	return function(scope, element, attrs){
+		element.bind("click", function(){
+			scope.$apply(attrs.callmethod)
+		})
+	}
+})
+
 tasks.controller("TasksController", function($scope, TaskFactory){
 	$scope.all_tasks = TaskFactory.list;
 	$scope.new_task = {}
@@ -8,9 +95,12 @@ tasks.controller("TasksController", function($scope, TaskFactory){
 	$scope.add_task = function(new_task){
 		TaskFactory.list.push(angular.copy(new_task))
 	}
+
+	$scope.load_task = function(){
+		alert("Loading more task...")
+	}
 })
 
-//model
 tasks.factory("TaskFactory", function(){
 	var items = {
 		list: [
